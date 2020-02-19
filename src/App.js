609f891336d6f1641a8208cce300be0f62ebe256
admin <sh.je@metanet.co.kr>
2020-02-19 16:32:00 +0900

    commit2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
+// import logo from './logo.svg';
 import './App.css';
+
+const customer = [{
+  'id':1
+ ,'image':'https://placeimg.com/64/64/1'
+ ,'name':'DHSeo'
+ ,'birthday':'961222'
+ ,'gender':'male'
+ ,'job':'student'
+},
+{
+  'id':2
+ ,'image':'https://placeimg.com//64/64/2'
+ ,'name':'JamesLee'
+ ,'birthday':'961223'
+ ,'gender':'male'
+ ,'job':'programer'
+}
+]
+
+// var i=0;
 
 class App extends Component {
   render() {
     return (
-    <div className="gray-background">
+      // <Customer
+      // id={customer[i].id}
+      // image={customer[i].image}
+      // name={customer[i].name}
+      // birthday={customer[i].birthday}
+      // gender={customer[i].gender}
+      // job={customer[i].job}
+      // />
+<div>
+  {customer.map(c=>{
+    return <Customer key={c.id} id={c.id} image={c.image} />
+  })}
+</div>
 
-        <img src={logo} alt="logo" />
-        <h2> New </h2>
-
-    </div>
     );
   }
 }
